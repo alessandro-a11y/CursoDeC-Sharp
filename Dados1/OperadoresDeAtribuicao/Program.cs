a21543f4b3808c0cd91c3dcbd9539fdd14e2c583
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-08 10:47:05 -0300

    Replace int assignment examples with string concatenation
    Commented out the integer assignment and arithmetic operator examples and added a string concatenation example using the += operator. This change demonstrates assignment operators with strings instead of integers.

--- Dados1/OperadoresDeAtribuicao/Program.cs
+++ Dados1/OperadoresDeAtribuicao/Program.cs
@@ -5,13 +5,21 @@ namespace OperadoresDeAtribuicao
     {
         static void Main(string[] args)
         {
-            int x = 10;
-            Console.WriteLine($"Valor inicial: {x}");
-            Console.WriteLine($"1° = {x += 5}");
-            Console.WriteLine($"2° = {x -= 3}");
-            Console.WriteLine($"3° = {x *= 2}");
-            Console.WriteLine($"4° = {x /= 4}");
-            Console.WriteLine($"5° = {x %= 3}");
+            string y = "Hello, ";
+            y += "World!";
+            Console.WriteLine(y);
+
+
+
+
+
+            //int x = 10;
+            //Console.WriteLine($"Valor inicial: {x}");
+            //Console.WriteLine($"1° = {x += 5}");
+            //Console.WriteLine($"2° = {x -= 3}");
+            //Console.WriteLine($"3° = {x *= 2}");
+            //Console.WriteLine($"4° = {x /= 4}");
+            //Console.WriteLine($"5° = {x %= 3}");
         }
     }
 }
