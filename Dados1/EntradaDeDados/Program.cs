cb47267573150a4a99bb81721fb53a087ff9ff13
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-08 09:20:10 -0300

    Add OperadoresAritmeticos project with arithmetic examples
    Created a new project 'OperadoresAritmeticos' demonstrating basic arithmetic operations in C#. Updated the solution file to include the new project. Also added real-world input/output examples to EntradaDeDados/Program.cs.

--- Dados1/EntradaDeDados/Program.cs
+++ Dados1/EntradaDeDados/Program.cs
@@ -29,4 +29,24 @@ namespace EntradaDeDados
             Console.ReadKey();
         }
     }
+}
+
+// exemplos usados na vida real
+using System;
+namespace EntradaDeDados
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Informe o seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe a sua idade:");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe a sua altura (em metros):");
+            float altura = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Olá, {nome}. Você tem {idade} anos de idade e {altura} metros de altura.");
+            Console.ReadKey();
+        }
+    }
 }*/
---
+++ Dados1/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace OperadoresAritmeticos
+{
+    internal class Progra
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor de x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Soma: {x+y}");
+            Console.WriteLine($"Subtração: {x-y}");
+            Console.WriteLine($"Multiplicação: {x*y}");
+            Console.WriteLine($"Divisão: {x/y}");
+            Console.WriteLine($"Módulo: {x%y}");
+        }
+    }
+}
