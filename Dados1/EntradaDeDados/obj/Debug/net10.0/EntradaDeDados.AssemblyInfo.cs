97cc294a7a0fe213c5027fc72ccb2882dc054ac2
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-08 10:23:10 -0300

    Add InterferenciaDeTipos project
    Created new project 'InterferenciaDeTipos' targeting .NET 10.0 with a basic Program.cs. Includes initial project and build files.

--- Dados1/EntradaDeDados/obj/Debug/net10.0/EntradaDeDados.AssemblyInfo.cs
+++ Dados1/EntradaDeDados/obj/Debug/net10.0/EntradaDeDados.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cf4d9bad737675add7d3da248fbf41a3dbe46a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb47267573150a4a99bb81721fb53a087ff9ff13")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Dados1/InterferenciaDeTipos/obj/Release/net10.0/InterferenciaDeTipos.AssemblyInfo.cs
+++ Dados1/InterferenciaDeTipos/obj/Release/net10.0/InterferenciaDeTipos.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Identificadores")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("InterferenciaDeTipos")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb47267573150a4a99bb81721fb53a087ff9ff13")]
-[assembly: System.Reflection.AssemblyProductAttribute("Identificadores")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Identificadores")]
+[assembly: System.Reflection.AssemblyProductAttribute("InterferenciaDeTipos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("InterferenciaDeTipos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
