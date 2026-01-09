034f74ec4fd9960421b20860d7e41de3bf5c3e2b
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-08 21:46:04 -0300

    Update Program.cs

--- Dados1/IncrementoEDecremento/Program.cs
+++ Dados1/IncrementoEDecremento/Program.cs
@@ -1,13 +1,35 @@
-﻿using System;
-namespace IncrementoEDecremento
+﻿//using System;
+//namespace IncrementoEDecremento
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int x = 10;
+//            x++;
+//            int y = 10;
+//            y--;
+//            Console.WriteLine(x);
+//            Console.WriteLine(y);
+//        }
+//    }
+//}
+using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            int x = 10;
-            x++;
-            Console.WriteLine(x);
-        }
+        int numero = 10;
+
+        // incremento
+        numero++;   // agora vale 11
+        ++numero;   // agora vale 12
+
+        // decremento
+        numero--;   // agora vale 11
+        --numero;   // agora vale 10
+
+        Console.WriteLine(numero);
     }
 }
