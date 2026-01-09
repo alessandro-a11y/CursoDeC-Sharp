2106abcc816ae4b9c608d6dfac4213bb79f6fb75
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-08 21:58:41 -0300

    Refactor EntradaDeDados and IncrementoEDecremento examples
    Commented out previous input and increment/decrement code examples in Program.cs files and replaced them with new or updated commented code blocks. This prepares the files for new examples or further development while preserving previous code for reference.

--- Dados1/EntradaDeDados/Program.cs
+++ Dados1/EntradaDeDados/Program.cs
@@ -1,52 +1,24 @@
-﻿/*using System;
-namespace EntradaDeDados
-{
-    internal class Program 
-    {
-        static void Main(string[] args)
-        {
-           Console.WriteLine("Informe o seu nome:");
-            string nome = Console.ReadLine();
-            Console.WriteLine($"Olá, {nome}");
-            Console.ReadKey();
-        }
+﻿//using System;
+//namespace EntradaDeDados
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Digite sua 1° nota: ");
+//            double nota1 = double.Parse(Console.ReadLine());
 
-    }
-}
+//            Console.WriteLine("Digite sua 2° nota: ");
+//            double nota2 = double.Parse(Console.ReadLine());
 
-using System;
-namespace EntradaDeDados
-{
-    internal class Program 
-    {
-        static void Main(string[] args)
-        {
-           Console.WriteLine("Informe o seu nome:");
-            string nome = Console.ReadLine();
-            Console.WriteLine("Informe a sua idade:");
-            int idade = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Olá, {nome}. Você tem {idade} anos de idade.");
-            Console.ReadKey();
-        }
-    }
-}
+//            Console.WriteLine("Digite sua 3° nota: ");
+//            double nota3 = double.Parse(Console.ReadLine());
 
-// exemplos usados na vida real
-using System;
-namespace EntradaDeDados
-{
-    internal class Program 
-    {
-        static void Main(string[] args)
-        {
-           Console.WriteLine("Informe o seu nome:");
-            string nome = Console.ReadLine();
-            Console.WriteLine("Informe a sua idade:");
-            int idade = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Informe a sua altura (em metros):");
-            float altura = float.Parse(Console.ReadLine());
-            Console.WriteLine($"Olá, {nome}. Você tem {idade} anos de idade e {altura} metros de altura.");
-            Console.ReadKey();
-        }
-    }
-}*/
+//            Console.WriteLine("Digite sua 4° nota: ");
+//            double nota4 = double.Parse(Console.ReadLine());
+
+//            double media = (double)(nota1 + nota2 + nota3 + nota4) / 4;
+//            Console.WriteLine($"Sua média: {media}");
+//        }
+//    }
+//}
--- Dados1/EntradaDeDados/obj/Debug/net10.0/EntradaDeDados.AssemblyInfo.cs
+++ Dados1/EntradaDeDados/obj/Debug/net10.0/EntradaDeDados.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac6251668d131a8480979e067cab731c5d760517")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+034f74ec4fd9960421b20860d7e41de3bf5c3e2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
