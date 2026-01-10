28c8a1195dcb3ca16f4d81d58193293b7ccf59d8
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-10 16:30:44 -0300

    Update Program.cs

--- Dados1/OperadoresLogicos/Program.cs
+++ Dados1/OperadoresLogicos/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace OperadoresLogicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = true;
+            bool c2 = false;
+            Console.WriteLine(c1&&c2);
+            Console.WriteLine(c1||c2);
+            Console.WriteLine(!c2);
+        }
+    }
+}
