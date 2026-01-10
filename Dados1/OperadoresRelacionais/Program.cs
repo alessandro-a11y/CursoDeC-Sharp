30671490d8789455b5d40a073eb6d659dbf8b4f1
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-10 16:30:49 -0300

    Update Program.cs

--- Dados1/OperadoresRelacionais/Program.cs
+++ Dados1/OperadoresRelacionais/Program.cs
@@ -1,24 +1,24 @@
-﻿using System;
-namespace OperadoresRelacionais
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int a = 10;
-            int b = 20;
-            Console.WriteLine("=============================================");
-            Console.WriteLine($"a = {a}, b = {b}");
-            Console.WriteLine($"a == b: {a == b}");
-            Console.WriteLine($"a != b: {a != b}");
-            Console.WriteLine($"a > b: {a > b}");
-            Console.WriteLine($"a < b: {a < b}");
-            Console.WriteLine($"a >= b: {a >= b}");
-            Console.WriteLine($"a <= b: {a <= b}");
-            Console.WriteLine("===============================================");
+﻿//using System;
+//namespace OperadoresRelacionais
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int a = 10;
+//            int b = 20;
+//            Console.WriteLine("=============================================");
+//            Console.WriteLine($"a = {a}, b = {b}");
+//            Console.WriteLine($"a == b: {a == b}");
+//            Console.WriteLine($"a != b: {a != b}");
+//            Console.WriteLine($"a > b: {a > b}");
+//            Console.WriteLine($"a < b: {a < b}");
+//            Console.WriteLine($"a >= b: {a >= b}");
+//            Console.WriteLine($"a <= b: {a <= b}");
+//            Console.WriteLine("===============================================");
 
-            Console.ReadKey();
+//            Console.ReadKey();
 
-        }
-    }
-}
+//        }
+//    }
+//}
