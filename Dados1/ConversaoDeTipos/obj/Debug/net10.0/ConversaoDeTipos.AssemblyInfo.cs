cc60687d998dc6c89d0752107a7041f50f0be916
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-10 16:58:20 -0300

    Update ConversaoDeTipos.AssemblyInfo.cs

--- Dados1/ConversaoDeTipos/obj/Debug/net10.0/ConversaoDeTipos.AssemblyInfo.cs
+++ Dados1/ConversaoDeTipos/obj/Debug/net10.0/ConversaoDeTipos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConversaoDeTipos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84fc29837dd132377322aaef91534f9c5f581956")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+770fa1b67998b5273797ea4d794c95b4baca063e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConversaoDeTipos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConversaoDeTipos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
