b8c3f29b587c2c68e53d253cad65c0cf09639919
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-10 16:59:05 -0300

    Update NullableTypes.AssemblyInfo.cs

--- Dados1/NullableTypes/obj/Debug/net10.0/NullableTypes.AssemblyInfo.cs
+++ Dados1/NullableTypes/obj/Debug/net10.0/NullableTypes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NullableTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84fc29837dd132377322aaef91534f9c5f581956")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+770fa1b67998b5273797ea4d794c95b4baca063e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NullableTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NullableTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
