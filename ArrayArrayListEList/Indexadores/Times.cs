83f25f15ba42a9020534ea825e714de2eefd4a70
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-16 11:40:00 -0300

    Create Times.cs

---
+++ ArrayArrayListEList/Indexadores/Times.cs
@@ -0,0 +1,27 @@
+﻿namespace Indexadores
+{
+    public class Times
+    {
+        string[] valor = new string[10];
+
+        public string this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < valor.Length)
+                {
+                    return valor[i];
+                }
+                return "error";
+            }
+            set
+            {
+                if (i >= 0 && i < valor.Length)
+                {
+                    valor[i] = value;
+                }
+               
+            }
+        }
+    }
+}
