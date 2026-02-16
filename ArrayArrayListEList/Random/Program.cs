a67cee4ef57d3bccd014ff261abbfd51aeec5d08
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-16 11:55:29 -0300

    Create Program.cs

---
+++ ArrayArrayListEList/Random/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Randomm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            Console.WriteLine("Número aleatório entre 0 e 100: " + random.Next(0, 101));
+            Console.WriteLine("Número aleatório entre 1 e 10: " + random.Next(1, 11));
+            Console.WriteLine("Número aleatório entre 50 e 150: " + random.Next(50, 151));
+        }
+    }   
+}
