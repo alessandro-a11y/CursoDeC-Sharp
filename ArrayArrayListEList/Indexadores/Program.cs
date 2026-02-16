f48a0b75b1ae3c7170a6dfec98bd35daca6b37f2
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-16 10:03:55 -0300

    chore: aplica gitignore corretamente

---
+++ ArrayArrayListEList/Indexadores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;   
+namespace Indexadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var minhaLista = new MinhaLista();
+            minhaLista[0] = "Primeiro item";
+            Console.WriteLine(minhaLista[0]);
+            minhaLista[1] = "Segundo item";
+            minhaLista[2] = "Terceiro item";
+            Console.WriteLine(minhaLista[1]);
+            Console.WriteLine(minhaLista[2]);
+        }
+    }
+    public class MinhaLista
+    {
+        private string[] dados = new string[10];
+
+        
+        public string this[int index]
+        {
+            get
+            {
+                return dados[index];
+            }
+            set
+            {
+                dados[index] = value;
+            }
+        }
+    }
+}
