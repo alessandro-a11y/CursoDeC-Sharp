204e4c3d45ea0e350f22ae07961d8aa6ffad8a6f
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-16 11:40:06 -0300

    Create ExercicioTimes.cs

---
+++ ArrayArrayListEList/Indexadores/ExercicioTimes.cs
@@ -0,0 +1,31 @@
+﻿using System;           
+using System.Collections.Generic;
+
+namespace Indexadores
+{
+    internal class ExercicioTimes
+    {
+        public static void Main(string[] args)
+        {
+            Times time = new Times();
+
+            time[0] = "Flamengo";
+            time[1] = "Vasco";
+            time[2] = "Botafogo";
+            time[3] = "Fluminense";
+            time[4] = "Bangu";
+            time[5] = "Madureira";
+            // sp
+            time[6] = "Palmeiras";
+            time[7] = "Santos";
+            time[8] = "São Paulo";
+            time[9] = "Corinthians";
+            time[10] = "Bragantino";
+
+            for (int j = 0; j < 10 ; j++)
+            {
+                Console.WriteLine(time[j]);
+            }
+        }
+    }
+}
