bb674d489a6e71c366be19af4ce913eaa134338f
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-17 12:32:45 -0300

    Create Pessoa.cs

---
+++ ArrayArrayListEList/ExercicioFinal/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿//namespace ExercicioFinal
+//{
+//    internal partial class ListaObjetosPessoas
+//    {
+//        public class Pessoa
+//        {
+//            public string Nome { get; set; }
+//            public int Idade { get; set; }
+//            public Pessoa(string nome, int idade)
+//            {
+//                Nome = nome;
+//                Idade = idade;
+//            }
+
+//            // Método atualizado: remove parâmetros inúteis e usa o campo estático `pessoas`
+//            public static void Exibir()
+//            {
+//                Console.WriteLine("\nLista de Pessoas:");
+//                foreach (var pessoa in pessoas)
+//                {
+//                    Console.WriteLine($"Nome: {pessoa.Nome}, Idade: {pessoa.Idade}");
+//                }
+//            }
+//        }
+//    }
+//}
