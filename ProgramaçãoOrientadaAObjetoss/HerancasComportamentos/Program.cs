0bd1dd182078ea427a4b1f7c6bed55521fbee0d8
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-18 19:48:50 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerancasComportamentos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HerancasComportamentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+  
+        }
+    }
+}
