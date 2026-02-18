8df42558abc282000ff2e817c1a1785181bf398b
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-18 15:05:43 -0300

    Create Pessoa.cs

---
+++ ProgramaçãoOrientadaAObjetoss/ProgramaçãoOrientadaAObjetoss/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace ProgramaçãoOrientadaAObjetoss
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public void Identificar()
+        {
+            Console.WriteLine($"{Nome} -- {Email}");
+
+        }
+    }
+}
