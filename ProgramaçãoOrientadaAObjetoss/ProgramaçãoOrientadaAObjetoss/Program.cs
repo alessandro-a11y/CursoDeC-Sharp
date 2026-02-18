7a1d1bfd61e75106fb560b0318cb6a99a54fb548
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-18 15:05:46 -0300

    Update Program.cs

--- ProgramaçãoOrientadaAObjetoss/ProgramaçãoOrientadaAObjetoss/Program.cs
+++ ProgramaçãoOrientadaAObjetoss/ProgramaçãoOrientadaAObjetoss/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ProgramaçãoOrientadaAObjetoss
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            funcionario.Nome = "João";
+            funcionario.Email = "teste@gmail.com";
+            funcionario.Empresa = "Microsoft";
+            funcionario.Salario = 5000;
+
+            Console.WriteLine("---FUNCIÓNÁRIO---");
+            funcionario.Identificar();
+            Console.WriteLine($"Empresa: {funcionario.Empresa}");
+            Console.WriteLine($"Salário: {funcionario.Empresa}");
+            ;
+
+
+            Aluno aluno = new Aluno();
+            aluno.Nome = "Maria";
+            aluno.Email = "Maria@Gmail.com";
+            aluno.Curso = "Engenharia de Software";
+            aluno.Nota = 10;
+
+            Console.WriteLine("\n---ALUNO---");
+            aluno.Identificar();
+            Console.WriteLine($"Curso: {aluno.Curso}");
+            Console.WriteLine($"Nota: {aluno.Nota}");
+        }
+    }
+}
