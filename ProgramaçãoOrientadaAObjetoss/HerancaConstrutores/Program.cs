f8f559e4c1ce20995104ab526b74da7dba1e5198
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-20 15:00:10 -0300

    Update Program.cs

--- ProgramaçãoOrientadaAObjetoss/HerancaConstrutores/Program.cs
+++ ProgramaçãoOrientadaAObjetoss/HerancaConstrutores/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace HerancaConstrutores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Aluno aluno = new Aluno("Alessandro");
+        }
+    }
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+            Console.WriteLine("Construtor da classe Pessoa");
+        }
+        public Pessoa(string nome)
+        {
+            Console.WriteLine("Construtor da classe Pessoa com parâmetro: " + nome);
+        }
+    }
+    // derivada
+    public class Aluno : Pessoa
+    {
+        public Aluno()
+        {
+            Console.WriteLine("Construtor da classe Aluno");
+        }
+        public Aluno(string nome)
+        {
+             Console.WriteLine("Construtor da classe Aluno com parâmetro: " + nome);
+        }
+    }
+}
