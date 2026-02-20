d91cfbd8236172aad7d36d85d03b165b85a5db2e
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-19 23:29:16 -0300

    Update Program.cs

--- ProgramaçãoOrientadaAObjetoss/HerancasComportamentos/Program.cs
+++ ProgramaçãoOrientadaAObjetoss/HerancasComportamentos/Program.cs
@@ -1,14 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace HerancasComportamentos
+class Animal
 {
-    internal class Program
+    protected string nome;
+
+    public Animal(string nome)
     {
-        static void Main(string[] args)
-        {
-           
-  
-        }
+        this.nome = nome;
+    }
+
+    protected void EmitirSom()
+    {
+        Console.WriteLine("O animal está emitindo um som...");
+    }
+}
+
+class Cachorro : Animal
+{
+    public Cachorro(string nome) : base(nome) { }
+
+    public void Latir()
+    {
+        Console.WriteLine($"{nome} está latindo!");
+        EmitirSom(); // Pode acessar porque é protected
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Cachorro dog = new Cachorro("Rex");
+        dog.Latir();
+
+        // dog.nome ❌ ERRO (não pode acessar)
+        // dog.EmitirSom() ❌ ERRO (não pode acessar)
     }
 }
