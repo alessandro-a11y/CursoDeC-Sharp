acac516a1b7f51c9182c24eb67d840cde1ee6bfa
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-21 20:20:48 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerancaNew/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HerancaNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new();
+            aluno.Nome = "João";
+            aluno.Curso = "Engenharia de Software";
+            Console.WriteLine(aluno.Saudacao());
+        }
+    }
+    public class Pessoa
+    {
+        public string? Nome { get; set; }
+        public string Saudacao() => $"Olá, meu nome é {Nome}";
+    }
+    public class Aluno : Pessoa
+    {
+        public string? Curso { get; set; }
+        public new string Saudacao() => $"Olá, meu nome é {Nome} e estou cursando {Curso}";
+    }
+}
