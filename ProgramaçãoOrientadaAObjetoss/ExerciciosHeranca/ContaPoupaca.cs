28d92b8492c9d9fa775939e9fa76f5be1a895161
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-21 20:21:04 -0300

    Create ContaPoupaca.cs

---
+++ ProgramaçãoOrientadaAObjetoss/ExerciciosHeranca/ContaPoupaca.cs
@@ -0,0 +1,18 @@
+﻿namespace ExerciciosHeranca
+{
+    class ContaPoupaca : Conta
+    {
+        public override void DepositarValor(decimal valor)
+        {
+            base.DepositarValor(valor);
+
+            if (valor > 0)
+            {
+                decimal rendimento = valor * 0.005m;
+                saldo += rendimento;
+
+                Console.WriteLine($"Seu rendimento: {rendimento}");
+            }
+        }
+    }
+}
