2989dae0fb7e0cc0249f53e48dd41de6a08e54d6
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-21 20:20:56 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/ExerciciosHeranca/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExerciciosHeranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta[] contas = {
+        new ContaCorrente(),
+        new ContaPoupaca(),
+        new ContaInvestimento()
+    };
+
+            foreach (var conta in contas)
+            {
+                Console.WriteLine($"--- {conta.GetType().Name} ---");
+
+                conta.DepositarValor(1000);
+                conta.Sacar(100);
+                conta.ExibirSaldo();
+
+                conta.Sacar(2000);
+                conta.ExibirSaldo();
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
