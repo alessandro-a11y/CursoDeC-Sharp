b04b8d1e5161dbbf133ffe5fdecdc0bd60f8f796
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-21 20:20:34 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerancaVirtualOverride/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HerancaOverrideVirtual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa { Nome = "Maria" };
+            Console.WriteLine(pessoa.Saudacao());
+        }
+    }
+    public class Pessoa
+    {
+        public string? Nome { get; set; }
+        public virtual string Saudacao() => $"Olá, meu nome é {Nome}";
+    }
+    public class Aluno : Pessoa
+    {
+        public string? Curso { get; set; }
+        public override string Saudacao() => $"Olá, meu nome é {Nome} e estou cursando {Curso}";
+    }
+}
