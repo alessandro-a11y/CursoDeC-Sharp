c126bed491c3ad5da82d7506619d5592c813d45b
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-22 19:51:57 -0300

    Create Quadrado.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerancaMultipla/Quadrado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaMultipla
+{
+    internal class Quadrado : Forma, ICusto
+    {
+        public int GetArea()
+        {
+            return (lado * lado);
+        }
+        public int GetCusto(int area)
+        {
+            return area * 10;
+        }
+    }
+}
