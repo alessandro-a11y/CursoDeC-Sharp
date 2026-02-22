9f728a4c4e2864787114b04b8b0a3fc3bd644fe8
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-22 19:52:08 -0300

    Create ICusto.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerancaMultipla/ICusto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaMultipla
+{
+    internal interface ICusto
+    {
+        int GetCusto(int area);
+    }
+}
