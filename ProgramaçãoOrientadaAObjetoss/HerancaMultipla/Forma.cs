987930d66db1516e092cbab1407198751a44dae6
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-22 19:52:18 -0300

    Create Forma.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerancaMultipla/Forma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaMultipla
+{
+    internal class Forma
+    {
+        public void SetLado(int a)
+        {
+            lado = a;
+        }
+        protected int lado;
+    }
+}
