89a20a3d62c10c11b80d8154b2b52439ee96b5fa
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-24 13:51:02 -0300

    Update Forma.cs

--- ProgramaçãoOrientadaAObjetoss/UpCasting/Forma.cs
+++ ProgramaçãoOrientadaAObjetoss/UpCasting/Forma.cs
@@ -6,5 +6,12 @@ namespace UpCasting
 {
     internal class Forma
     {
+        protected int xpos, ypos;
+        public Forma(int x, int y)
+        {
+            xpos = x;
+            ypos = y;
+        }
+        public virtual void Desenhar(10, 20) => Console.WriteLine($"Desenhando forma base em ({xpos}, {ypos})");
     }
 }
