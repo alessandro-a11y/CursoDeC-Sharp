4d523d9fe70e7e4d0c1a31f31c3f9de891697af7
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-24 13:51:16 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/OperadorAs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace OperadorAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Object carroObj = new Carro();
+
+            var y = carroObj as string;
+
+            if (y != null)
+            {
+                Console.WriteLine("Convertido para object");
+            }
+            else
+            {
+                Console.WriteLine("Y é null");
+            }
+        }
+    }
+    class Carro 
+    { }
+
+}
