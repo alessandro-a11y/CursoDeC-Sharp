77263302acfc3437a919cf5fc2ae318adf5d544c
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-27 12:10:30 -0300

    Create Forma.cs

---
+++ ProgramaçãoOrientadaAObjetoss/ModificadorAbstract/Forma.cs
@@ -0,0 +1,18 @@
+﻿namespace ModoficadorAbstract
+{
+    public abstract class Forma
+    {
+        public string Cor { get; set; }
+        public double Area { get; set; }
+        public double Perimetro { get; set; }
+
+        public abstract void CalcularArea();
+        public abstract void CalcularPerimetro();
+
+        public void Descriao()
+        {
+            Console.WriteLine($"Cor: {Cor}, Área: {Area}, Perímetro: {Perimetro}");
+
+        }
+    }
+}
