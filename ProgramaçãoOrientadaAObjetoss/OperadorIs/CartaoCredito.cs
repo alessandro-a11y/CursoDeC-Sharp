d166210d4b12bbaf5dad5e6385b57c005a601adc
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-26 23:31:47 -0300

    Create CartaoCredito.cs

---
+++ ProgramaçãoOrientadaAObjetoss/OperadorIs/CartaoCredito.cs
@@ -0,0 +1,7 @@
+﻿
+// Classes Filhas
+public class CartaoCredito : Pagamento
+{
+    public string NumeroCartao { get; set; }
+    public void ProcessarEstorno() => Console.WriteLine("Estorno solicitado para o cartão.");
+}
