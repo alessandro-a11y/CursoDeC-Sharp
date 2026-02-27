230b35423dc97133ed88c741c89ea602cc1bf3b6
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-27 12:10:20 -0300

    Create Quadrado.cs

---
+++ ProgramaçãoOrientadaAObjetoss/ModificadorAbstract/Quadrado.cs
@@ -0,0 +1,17 @@
+﻿namespace ModoficadorAbstract
+{
+    public class Quadrado : Forma
+    {
+        public double Lado { get; set; }
+        public override void CalcularArea()
+        {
+            Area = Lado * Lado;
+            Console.WriteLine($"Valor da Area: {Area}");
+        }
+        public override void CalcularPerimetro()
+        {
+            Perimetro = 4 * Lado;
+            Console.WriteLine($"Valor do Perimetro: {Perimetro}");
+        }
+    }
+}
