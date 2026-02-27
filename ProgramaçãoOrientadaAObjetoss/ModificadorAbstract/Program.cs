a7dd9626f414d98975391f3e7a119672e7c09dc1
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-27 12:10:25 -0300

    Update Program.cs

--- ProgramaçãoOrientadaAObjetoss/ModificadorAbstract/Program.cs
+++ ProgramaçãoOrientadaAObjetoss/ModificadorAbstract/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ModoficadorAbstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadrado = new Quadrado();
+            quadrado.Cor = "Vermelho";
+            quadrado.Lado = 5;
+            quadrado.CalcularArea();
+            quadrado.CalcularPerimetro();
+            quadrado.Descriao();
+
+        }
+    }
+}
