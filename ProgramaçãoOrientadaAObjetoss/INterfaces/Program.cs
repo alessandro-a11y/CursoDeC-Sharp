94c02b68435a546ab63ab598b15d1d0d18c5c009
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-01 12:18:00 -0300

    Update Program.cs

--- ProgramaçãoOrientadaAObjetoss/INterfaces/Program.cs
+++ ProgramaçãoOrientadaAObjetoss/INterfaces/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace INterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IControle controle = new Demo();
+            controle.Desenhar();
+            controle.Exibir();
+
+        }
+    }
+    interface IControle
+    {
+        void Desenhar();
+
+        public void Exibir()
+        {
+            Console.WriteLine("Exibindo");
+        }
+    }
+    public class Demo : IControle
+    {
+        public void Desenhar()
+        {
+            Console.WriteLine("Dsenhando");
+        }
+    }
+    interface IGrafico
+    {
+        void Desenhar();
+    }
+     public class Grafico : IGrafico
+    {
+        public void Desenhar()
+        {
+            Console.WriteLine("Dsenhando");
+        }
+    }
+}
