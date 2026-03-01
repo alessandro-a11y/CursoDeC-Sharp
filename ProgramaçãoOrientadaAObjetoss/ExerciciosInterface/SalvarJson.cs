44c9d5e2802c3cf57acd778f36fae2d84edfa657
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-01 12:18:20 -0300

    Create SalvarJson.cs

---
+++ ProgramaçãoOrientadaAObjetoss/ExerciciosInterface/SalvarJson.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercicioInterface
+{
+    public class SalvarJson : ArquivoBase, ISalvar
+    {
+        public void Salvar()
+        {
+            Console.WriteLine("Salvando em JSON");
+        }
+        public override void Nome()
+        {
+            Console.WriteLine("Definir nome Json");
+        }
+        public void Compactar()
+        {
+            Console.WriteLine("Compactando arquivo JSON");
+        }
+    }
+}
