46c6b6c3242ce192a7d5017976005f69d22bb8a8
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-01 12:18:08 -0300

    Create SalvarXml.cs

---
+++ ProgramaçãoOrientadaAObjetoss/ExerciciosInterface/SalvarXml.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercicioInterface
+{
+    public class SalvarXml : ArquivoBase, ISalvar
+    {
+        public void Salvar()
+        {
+            Console.WriteLine("Salvando em XML");
+        }
+        public override void Nome()
+        {
+            Console.WriteLine("Definir nome XML");
+        }
+        public void Compactar()
+        {
+            Console.WriteLine("Compactando arquivo XML");
+        }
+    }
+}
