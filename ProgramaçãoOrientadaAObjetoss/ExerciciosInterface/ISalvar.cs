ff883541ff9ccdb9615640746b81b659fdd0fad7
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-01 12:18:27 -0300

    Create ISalvar.cs

---
+++ ProgramaçãoOrientadaAObjetoss/ExerciciosInterface/ISalvar.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioInterface
+{
+    interface ISalvar
+    {
+        // método abstrato
+        void Salvar();
+        // método não abstrato
+        public void Compactar();
+    }
+}
