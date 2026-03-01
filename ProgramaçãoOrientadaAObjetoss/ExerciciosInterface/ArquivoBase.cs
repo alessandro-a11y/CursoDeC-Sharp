03a20fb515a8165352829b527dc51f8ad2126b70
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-01 12:18:32 -0300

    Create ArquivoBase.cs

---
+++ ProgramaçãoOrientadaAObjetoss/ExerciciosInterface/ArquivoBase.cs
@@ -0,0 +1,11 @@
+﻿namespace ExercicioInterface
+{
+    // criar classe abstrata ArquivoBase e definir um método virtual Nome()
+    public abstract class ArquivoBase
+    {
+        public virtual void Nome()
+        {
+            Console.WriteLine("Nome do arquivo");
+        }
+    }
+}
