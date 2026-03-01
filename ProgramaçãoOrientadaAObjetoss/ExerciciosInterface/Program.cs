5ffe104f2478d2ed93e550fc15cd8eb1d7a82fb0
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-01 12:18:23 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/ExerciciosInterface/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ExercicioInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // testar a implementação das classes SalvarXml e SalvarJson
+            ISalvar salvarXml = new SalvarXml();
+            salvarXml.Salvar();
+            salvarXml.Compactar();
+            ISalvar salvarJson = new SalvarJson();
+            salvarJson.Salvar();
+            salvarJson.Compactar();
+        }
+    }
+}
