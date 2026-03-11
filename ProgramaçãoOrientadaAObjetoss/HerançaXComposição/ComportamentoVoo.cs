6ca3ebf2259b046282265f3255adb9cd65e6b40c
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:47 -0300

    Create ComportamentoVoo.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerançaXComposição/ComportamentoVoo.cs
@@ -0,0 +1,10 @@
+﻿namespace HerançaXComposição
+{
+    public class ComportamentoVoo : Animal
+    {
+        public void Voar()
+        {
+            Console.WriteLine("Voando...");
+        }
+    }
+}
