87e794e5dde16b7325018d883c375b80e91895b0
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:13:06 -0300

    Create LivroFotos.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Exercicio2/LivroFotos.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio2
+{
+    public class LivroFotos
+    {
+        private int numPaginas;
+
+        // construtor padrão
+        public LivroFotos()
+        {
+            numPaginas = 16;
+        }
+
+        // construtor com parâmetro
+        public LivroFotos(int numPaginas)
+        {
+            this.numPaginas = numPaginas;
+        }
+        public int GetNumeroPaginas()
+        {
+            return numPaginas;
+        }
+
+    }
+}
