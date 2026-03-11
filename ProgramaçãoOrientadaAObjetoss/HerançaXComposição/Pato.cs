058b9e4bbaa8e3b349aeeaa17af15db56b5146fe
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:30 -0300

    Create Pato.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerançaXComposição/Pato.cs
@@ -0,0 +1,16 @@
+﻿namespace HerançaXComposição
+{
+    public class Pato : Animal
+    {
+        private readonly ComportamentoVoo _comportamentoVoo;
+
+        public Pato(ComportamentoVoo comportamentoVoo)
+        {
+            _comportamentoVoo = comportamentoVoo;
+        }
+        public void Voar()
+        {
+            _comportamentoVoo.Voar();
+        }
+    }
+}
