885cabd500aea93c69acf9f41719e8e26f71918f
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:13:12 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Exercicio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro(0);
+            Console.WriteLine("Informe a quantidade de gasolina para abastecer: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            carro.Abastecer(quantidade);
+            carro.Dirigir();
+        }
+    }
+}
