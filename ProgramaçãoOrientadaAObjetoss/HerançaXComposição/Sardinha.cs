4ce74d312a511926ff80a62294c0e1110c2ad631
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:24 -0300

    Create Sardinha.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerançaXComposição/Sardinha.cs
@@ -0,0 +1,15 @@
+﻿namespace HerançaXComposição
+{
+    public class Sardinha : Animal
+    {
+        private readonly ComportamentoNadar _comportamentoNadar;
+        public Sardinha(ComportamentoNadar comportamentoNadar)
+        {
+            this._comportamentoNadar = comportamentoNadar;
+        }
+        public void Nadar()
+        {
+            _comportamentoNadar.Nadar();
+        }
+    }
+}
