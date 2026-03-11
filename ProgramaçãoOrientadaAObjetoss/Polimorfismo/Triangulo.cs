21da5f318d0fb9fc07751881627f81e43b202719
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:06 -0300

    Create Triangulo.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Polimorfismo/Triangulo.cs
@@ -0,0 +1,11 @@
+﻿namespace Polimorfismo
+{
+    public class Triangulo : Figura
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um triângulo.");
+            
+        }
+    }
+}
