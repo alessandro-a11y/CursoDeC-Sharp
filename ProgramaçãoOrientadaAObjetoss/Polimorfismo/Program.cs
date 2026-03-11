ca6c9b832a0b8f97c63286e813dca13edeebedd9
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:12 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Polimorfismo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var figuras = new List<Figura>
+            {
+                new Triangulo(),
+                new Circulo(),
+                new Quadrado()
+            };
+            foreach (var figura in figuras)
+            {
+                figura.Desenhar();
+            }
+        }
+    }
+}
