aed6ff6bc2354af3bf400f641dc85a8cf8904d32
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:13:21 -0300

    Create Carro.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Exercicio/Carro.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio
+{
+    public class Carro : IVeiculo 
+    {
+        public int QuantidadeInicial { get; set; }
+
+        public Carro(int quantidadeInicial)
+        {
+            QuantidadeInicial = quantidadeInicial;
+        }
+        public void Dirigir()
+        {
+            if (QuantidadeInicial > 0)
+            {
+                Console.WriteLine("Dirigindo...");
+                QuantidadeInicial--;
+            }
+            else
+            {
+                Console.WriteLine("Sem combustível");
+            }
+        }
+
+        public bool Abastecer(int quantidade)
+        {
+            QuantidadeInicial += quantidade;
+            return true;
+        }
+    }
+}
