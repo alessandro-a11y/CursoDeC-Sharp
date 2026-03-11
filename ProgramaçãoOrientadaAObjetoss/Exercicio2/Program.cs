0b34b2edd665041226b664480655ef395f7330b3
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:13:03 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Exercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LivroFotos album1 = new LivroFotos();
+            LivroFotos album2 = new LivroFotos(24);
+            SuperLivroFotos album3 = new SuperLivroFotos();
+
+            Console.WriteLine(album1.GetNumeroPaginas());
+            Console.WriteLine(album2.GetNumeroPaginas());
+            Console.WriteLine(album3.GetNumeroPaginas());
+        }
+    }
+}
