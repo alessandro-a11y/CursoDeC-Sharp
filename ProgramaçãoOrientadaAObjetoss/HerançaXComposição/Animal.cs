649964494d16482a5f4a1f83a1e9529705fdfe62
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:55 -0300

    Create Animal.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerançaXComposição/Animal.cs
@@ -0,0 +1,15 @@
+﻿namespace HerançaXComposição
+{
+    public class Animal
+    {
+        public int idade { get; set; }
+        public void Comer()
+        {
+            Console.WriteLine("Comendo...");
+        }
+        public void Dormir()
+        {
+            Console.WriteLine("Dormindo...");
+        }
+    }
+}
