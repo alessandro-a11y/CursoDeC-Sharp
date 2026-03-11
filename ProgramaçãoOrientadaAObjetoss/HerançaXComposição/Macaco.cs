da1d1d0845b27ab14627b36bceb9e7116ee1edef
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:33 -0300

    Create Macaco.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerançaXComposição/Macaco.cs
@@ -0,0 +1,16 @@
+﻿namespace HerançaXComposição
+{
+    public class Macaco : Animal
+    {
+        private readonly ComportamentoAndar _comportamentoAndar;
+
+        public Macaco(ComportamentoAndar comportamentoAndar)
+        {
+            this._comportamentoAndar = comportamentoAndar;
+        }
+        public void Andar()
+        {
+            _comportamentoAndar.Andar();
+        }
+    }
+}
