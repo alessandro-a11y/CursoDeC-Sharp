01a484508f3a326d794488dd87ec28565e330c2a
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:35 -0300

    Create Homem.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerançaXComposição/Homem.cs
@@ -0,0 +1,16 @@
+﻿namespace HerançaXComposição
+{
+    public class Homem : Animal
+    {
+        private readonly ComportamentoAndar _comportamentoAndar;
+
+        public Homem(ComportamentoAndar comportamentoAndar)
+        {
+            this._comportamentoAndar = comportamentoAndar;
+        }
+        public void Andar()
+        {
+            _comportamentoAndar.Andar();
+        }
+    }
+}
