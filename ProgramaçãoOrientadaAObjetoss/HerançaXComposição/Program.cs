d5ce935f03180b78c3d13cba4cc5c69d43b98a55
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:27 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/HerançaXComposição/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace HerançaXComposição
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComportamentoAndar comportamentoAndar = new ComportamentoAndar();
+            ComportamentoNadar comportamentoNadar = new ComportamentoNadar();
+            ComportamentoVoo  comportamentoVoo = new ComportamentoVoo();
+
+            Macaco macaco = new Macaco(comportamentoAndar);
+            macaco.Andar();
+            Homem homem = new Homem(comportamentoAndar);
+            homem.Andar();
+            Pato pato = new Pato(comportamentoVoo);
+            pato.Voar();
+            Sardinha sardinha = new Sardinha(comportamentoNadar);
+            sardinha.Nadar();
+
+        }
+    }
+}
