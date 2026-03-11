59a4dc8088d3e6eed9681b51dca0d70aac44f28c
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:22 -0300

    Create Circulo.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Polimorfismo/Circulo.cs
@@ -0,0 +1,11 @@
+﻿namespace Polimorfismo
+{
+    public class Circulo : Figura
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um círculo.");
+            
+        }
+    }
+}
