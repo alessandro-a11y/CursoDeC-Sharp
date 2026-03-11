4463899634723f756881d61a44c8ec78be92bcbe
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-11 15:12:17 -0300

    Create Figura.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Polimorfismo/Figura.cs
@@ -0,0 +1,10 @@
+﻿namespace Polimorfismo
+{
+    public class Figura
+    {
+        public virtual void Desenhar()
+        {
+            Console.WriteLine("Desenhando na classe base.");
+        }
+    }
+}
