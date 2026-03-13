2c101d313b2300f81f6fc295a0d0a14f04be66f0
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-13 10:37:06 -0300

    Create Program.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Exercicio5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Forma q = new Quadrado(5);
+        Forma t = new Triangulo(4, 6);
+        Forma c = new Circulo(3);
+
+        Console.WriteLine("Área do Quadrado: " + q.CalcularArea());
+        Console.WriteLine("Área do Triângulo: " + t.CalcularArea());
+        Console.WriteLine("Área do Círculo: " + c.CalcularArea());
+    }
+}
+
+abstract class Forma
+{
+    public abstract double CalcularArea();
+}
+
+class Quadrado : Forma
+{
+    double lado;
+
+    public Quadrado(double lado)
+    {
+        this.lado = lado;
+    }
+
+    public override double CalcularArea()
+    {
+        return lado * lado;
+    }
+}
+
+class Triangulo : Forma
+{
+    double baseTri;
+    double altura;
+
+    public Triangulo(double b, double h)
+    {
+        baseTri = b;
+        altura = h;
+    }
+
+    public override double CalcularArea()
+    {
+        return (baseTri * altura) / 2;
+    }
+}
+
+class Circulo : Forma
+{
+    double raio;
+
+    public Circulo(double r)
+    {
+        raio = r;
+    }
+
+    public override double CalcularArea()
+    {
+        return Math.PI * raio * raio;
+    }
+}
