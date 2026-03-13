c9ce6b7bcfd4ee11205169afd599ccafa6138765
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-13 10:24:35 -0300

    Create Pessoa.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Exercicio3/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicio3
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
