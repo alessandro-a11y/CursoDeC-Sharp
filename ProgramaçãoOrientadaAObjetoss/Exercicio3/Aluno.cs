927215c8c2c15a2d8765c848799483e0913a02cb
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-13 10:24:25 -0300

    Create Aluno.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Exercicio3/Aluno.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio3
+{
+    public class Aluno : Pessoa
+    {
+        public Aluno(string nome) : base(nome)
+        {
+        }
+
+        public void Estudar()
+        {
+            Console.WriteLine($"{Nome} está estudando.");
+        }
+    }
+}
