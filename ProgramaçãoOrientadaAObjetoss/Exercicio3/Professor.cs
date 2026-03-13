35fa07342d43e8e2bfb7baf1c1f9b03339517e7e
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-13 10:24:39 -0300

    Create Professor.cs

---
+++ ProgramaçãoOrientadaAObjetoss/Exercicio3/Professor.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio3
+{
+    public class Professor : Pessoa
+    {
+        public Professor(string nome) : base(nome)
+        {
+        }
+        public void Ensinar()
+        {
+            Console.WriteLine($"{Nome} está ensinando.");
+        }
+    }
+}
