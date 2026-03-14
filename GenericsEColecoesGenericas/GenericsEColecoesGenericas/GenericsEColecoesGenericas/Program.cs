042e3f387a0afd1ba7229b3120d108a5e959e50a
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-14 14:52:39 -0300

    Update Program.cs

--- GenericsEColecoesGenericas/GenericsEColecoesGenericas/GenericsEColecoesGenericas/Program.cs
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/GenericsEColecoesGenericas/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Teste teste = new Teste();
+        int i1 = 10;
+        int i2 = 20;
+
+        teste.Comparar(i1, i2);
+
+
+    }
+    public class Teste
+    {
+        public void Comparar<T> (T p1,  T p2)
+        {
+            var resultado = p1.Equals(p2);
+            Console.WriteLine($"{p1} {p2} são iguais ? {resultado}");
+        }
+    }
+}
