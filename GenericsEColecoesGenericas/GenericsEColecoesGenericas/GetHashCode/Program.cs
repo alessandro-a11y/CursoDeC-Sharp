889a817299ce070b6367962b93e7d29aa610d5a5
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-17 09:10:34 -0300

    Create Program.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/GetHashCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace GetHashCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "José";
+            string b = "José";
+
+            int x = 100;
+            int y = 100;
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(x.Equals(y));
+        }
+    }
+
+    public class Pessoa
+    {
+        
+    }
+}
