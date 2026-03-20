53d3b59f793f249d7e9d02a4a4c742ed3839ff79
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-20 10:02:42 -0300

    Create Program.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/Dictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;   
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dic3 = new Dictionary<int, int>()
+         {
+             {1, 100 },
+             {2, 200 },
+             {3, 300 }
+
+         };
+            Console.WriteLine("" +
+                "Incluir elemento com chave" +
+                "(3) duplicada");
+
+            try
+            {
+                dic3.Add(3, 300);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.ToString);
+            }
+   
+        }
+    }
+}
