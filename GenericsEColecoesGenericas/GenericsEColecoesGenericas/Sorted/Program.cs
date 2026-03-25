66a3f720310378d81111bc9db4ef36aec8097685
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-25 10:58:38 -0300

    Create Program.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/Sorted/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Sorted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var brics = new SortedDictionary<int, string>()
+            {
+                {  3, "Brasil"  },
+                { 1, "Rússia" },
+                { 4, "Índia" },
+                { 2, "China" }
+            };
+            exibir1(brics);
+
+            Console.WriteLine("--------------------------------------");
+            if (!brics.ContainsKey(5))
+            {
+                brics.Add(5, "África do Sul");
+                Console.WriteLine("\nÁfrica do Sul adicionada\n");
+                exibir1(brics);
+
+            }
+            Console.WriteLine("--------------------------------------");
+        }
+
+        private static void exibir1(SortedDictionary<int, string> brics)
+        {
+            foreach (var item2 in brics)
+            {
+                Console.WriteLine($"{item2.Key} - {item2.Value}");
+            }
+        }
+    }
+}
