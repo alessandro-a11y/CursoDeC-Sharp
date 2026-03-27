5867865162c7267f3d6e27a3e1f707f23d7a7718
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-27 11:34:22 -0300

    Create Program.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/ColecoesSet/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ColecoesSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        var numeros = new HashSet<int>() { 1, 2, 3, 4, 5 };
+
+        var frutas = new HashSet<string>() { "banana", "maçã", "laranja" };
+
+            HashSet<int> pares = new HashSet<int>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                pares.Add(i * 2);
+            }
+
+        var lista1 = new List<int>()
+        {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        };
+        var conjunto = new HashSet<int>(lista1);
+
+        var lista2 = new List<string>
+        {
+                "uva", "abacaxi", "melancia", "banana"
+        };
+            HashSet<string> minhasFrutas = new HashSet<string>(lista2);
+        }
+    }
+}
