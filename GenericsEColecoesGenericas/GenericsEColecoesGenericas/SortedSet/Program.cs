cd4cc60a6183fc43c75f73ac323afc876b931af8
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-27 11:34:10 -0300

    Create Program.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/SortedSet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ColecoesSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
