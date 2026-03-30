3e14908493963561008b65c71575f74e09cf55d5
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-30 09:30:24 -0300

    Create Program.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/Stack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numeros = new Stack<int>();
+            numeros.Push(1);
+
+
+            int[] array1 = new int[] { 2, 3, 4, 5 };
+            Stack<int> pilha1 = new Stack<int>(array1);
+
+            var impares = new Stack<int>(3);
+            impares.Push(5);
+
+        }
+    }
+}
