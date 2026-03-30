62e41e019179ed0a1434dacefedb45f71f0f267c
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-30 09:30:41 -0300

    Create Program.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/EditordeTexto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EditordeTexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EditorTexto editor = new EditorTexto();
+            Console.WriteLine("\nDigitando\n");
+            editor.DigitarChar('H');
+            editor.DigitarChar('e');
+            editor.DigitarChar('l');
+            editor.DigitarChar('l');
+            editor.DigitarChar('o');
+
+            Console.WriteLine("\nFazendo o Undo\n");
+            editor.Undo();
+            editor.Undo();
+
+
+            Console.WriteLine("\nRedigitando\n");
+            editor.DigitarChar('o');
+            editor.DigitarChar('o');
+            editor.DigitarChar('!');
+        }
+    }
+    public class EditorTexto
+    {
+        private Stack<char> undoStack = new Stack<char>();
+        private string texto = "";
+
+        public void DigitarChar(char c)
+        {
+            texto += c;
+            undoStack.Push(c);
+            Console.WriteLine($"Texto: {texto}");
+        }
+    
+    public void Undo()
+        {
+            if (undoStack.Count > 0)
+            {
+                char ultimoChar = undoStack.Pop();
+                texto = texto.Substring(0, texto.Length - 1);
+                Console.WriteLine($"Texto: {texto}");
+            }
+        }
+}   }
