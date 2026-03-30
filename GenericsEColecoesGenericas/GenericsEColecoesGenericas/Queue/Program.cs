362fdc63244fb9b91f10904c43c874eb507d48dd
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-30 10:31:46 -0300

    Create Program.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/Queue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    internal class Program : ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> fila = new Queue<string>();
+            fila.Enfileirar("Primeiro");
+            fila.Enfileirar("Segundo");
+            fila.Enfileirar("Terceiro");
+            Console.WriteLine($"Primeiro elemento: {fila.Primeiro()}");
+            Console.WriteLine($"Tamanho da fila: {fila.Tamanho()}");
+            Console.WriteLine($"Desenfileirando: {fila.Desenfileirar()}");
+            Console.WriteLine($"Primeiro elemento após desenfileirar: {fila.Primeiro()}");
+
+            string[] cursos = { "C#", "Java", "Python", "JavaScript" };
+            Queue<string> filaCursos = new Queue<string>();
+            Console.WriteLine();
+            Console.WriteLine("No. de Elementos na fila 1 : " + filaCursos.Tamanho());
+
+            ExibirCurso(cursos);
+
+        }
+    }
+}
