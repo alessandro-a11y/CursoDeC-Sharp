0d9979344e9b5317a11caf6d3c4079699e32ad2d
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-30 10:31:41 -0300

    Create ProgramBase.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/Queue/ProgramBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Queue
+{
+    internal class ProgramBase
+    {
+
+        private static void ExibirCurso<T>(IEnumerable<T> cursos)
+        {
+            Console.WriteLine();
+            foreach (var curso in cursos)
+            {
+                Console.WriteLine(curso);
+            }
+        }
+    }
+}
