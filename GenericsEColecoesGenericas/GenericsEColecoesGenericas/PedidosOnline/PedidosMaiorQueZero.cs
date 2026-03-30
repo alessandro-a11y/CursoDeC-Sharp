adffb044347379e5be3660de783c6756677a4fbe
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-30 10:31:59 -0300

    Create PedidosMaiorQueZero.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/PedidosOnline/PedidosMaiorQueZero.cs
@@ -0,0 +1,15 @@
+﻿namespace PedidosOnline
+{
+    internal class PedidosMaiorQueZero
+    {
+
+        public static void PedidosMaioresqueZero(Queue<Pedido> filaPedidos)
+        {
+            while (filaPedidos.Count > 0)
+            {
+                Pedido pedidoAtual = filaPedidos.Dequeue();
+                Console.WriteLine($"Processando pedido #{pedidoAtual.Numero} - Valor: R${pedidoAtual.Valor}");
+            }
+        }
+    }
+}
