f102c49b2b55795d64812a0605f3560e2e1921a3
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-30 10:32:03 -0300

    Create Pedido.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/PedidosOnline/Pedido.cs
@@ -0,0 +1,17 @@
+﻿namespace PedidosOnline
+{
+    internal partial class Program
+    {
+        class Pedido
+        {
+            public int Numero { get; set; }
+            public double Valor { get; set; }
+
+            public Pedido(int numero, double valor)
+            {
+                Numero = numero;
+                Valor = valor;
+            }
+        }
+    }
+}
