d6f2a57fc85b646af972cca15d9c02a229dd45c3
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-30 09:30:29 -0300

    Update Program.cs

--- GenericsEColecoesGenericas/GenericsEColecoesGenericas/SortedSet/Program.cs
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/SortedSet/Program.cs
@@ -6,7 +6,30 @@ namespace ColecoesSet
     {
         static void Main(string[] args)
         {
+            var numeros = new List<int>()
+            {
+                1, 7, 3, 6, 5, 4, 2
+            };
+            SortedSet<int> numerosOrdenados = new SortedSet<int>(numeros);
+            ExibirColecao(numerosOrdenados);
+
+            var frutas = new List<string>() { "uva", "banana", "abacaxi", "laranja" };
+
+            SortedSet<string> frutasOrdenadas = new SortedSet<string>(frutas);
+            ExibirColecao(frutasOrdenadas);
+
+            var impares = new int[] { 1, 3, 5, 7, 9 };
+            SortedSet<int> numerosImpares = new SortedSet<int>(impares);
+            ExibirColecao(numerosImpares);
 
         }
+        static void ExibirColecao<T>(IEnumerable<T> colecao)
+        {
+            foreach (var item in colecao)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
+
 }
