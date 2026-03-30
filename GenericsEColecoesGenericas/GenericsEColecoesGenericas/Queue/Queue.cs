0375defe801991303533ed2f571ee0e84894a014
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-30 10:31:31 -0300

    Create Queue.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/Queue/Queue.cs
@@ -0,0 +1,29 @@
+﻿namespace Queue
+{
+    public class Queue<T>
+    {
+        private LinkedList<T> elementos = new LinkedList<T>();
+        public void Enfileirar(T item)
+        {
+            elementos.AddLast(item);
+        }
+        public T Desenfileirar()
+        {
+            if (elementos.Count == 0)
+                throw new InvalidOperationException("A fila está vazia.");
+            T primeiroItem = elementos.First.Value;
+            elementos.RemoveFirst();
+            return primeiroItem;
+        }
+        public T Primeiro()
+        {
+            if (elementos.Count == 0)
+                throw new InvalidOperationException("A fila está vazia.");
+            return elementos.First.Value;
+        }
+        public int Tamanho()
+        {
+            return elementos.Count;
+        }
+    }
+}
