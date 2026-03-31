fc48527275cccb69502a7c9c2a751786cfa651df
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-03-31 18:34:30 -0300

    Create Program.cs

---
+++ GenericsEColecoesGenericas/GenericsEColecoesGenericas/Atividade1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Atividade1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Escreva um programa 
+            para adicionar dois números 
+            inteiros usando o conceito de Generics.
+             */
+
+            var calculadora = new Calculadora<int>();
+            calculadora.Adicionar(5, 10);
+
+        }
+    }
+    class Calculadora<T> where T : INumber<T>
+    {
+        public T Adicionar(T a, T b)
+        {
+            return a + b;
+        }
+    }
+}
