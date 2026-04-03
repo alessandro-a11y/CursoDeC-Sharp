72a368f48ad0d949fe2db6625443425814d5d9e4
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-03 20:52:47 -0300

    Create Program.cs

---
+++ TratamentoDeExceções/TratamentoDeExcecoes/TratamentoDeExcecoes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TratamentoDeExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite um número inteiro:");
+                int numero = int.Parse(Console.ReadLine());
+                Console.WriteLine($"O número digitado foi: {numero}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Erro: O valor digitado não é um número inteiro válido.");
+                Console.WriteLine($"Detalhes do erro: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocorreu um erro inesperado.");
+                Console.WriteLine($"Detalhes do erro: {ex.Message}");
+            }
+        }
+    }
+}
