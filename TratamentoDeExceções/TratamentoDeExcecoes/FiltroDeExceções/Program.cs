f9cd2ff8ac94d1f8762919af24449be63309dcb1
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-04 15:06:34 -0300

    Create Program.cs

---
+++ TratamentoDeExceções/TratamentoDeExcecoes/FiltroDeExceções/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FiltroDeExcecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("\nInforme o dividendo");
+                int dividendo = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nInforme o divisor");
+                int divisor = int.Parse(Console.ReadLine());
+
+                int resultado = dividendo / divisor;
+                Console.WriteLine($"\n{dividendo} / {divisor} = {resultado}\n");
+                Console.ReadKey();
+            }
+            catch (DivideByZeroException) when (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                {
+                    Console.WriteLine("\nSegunda amigão");
+                }
+            }
+            catch (Exception ex) when (ex.Message.Contains("format"))
+            {
+
+                Console.WriteLine("filtro de exceção com format");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nTratando o erro em Main\n");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
