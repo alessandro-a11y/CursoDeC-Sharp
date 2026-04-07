64927ff89b2cc7dd677d7e637525321681cae425
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-06 23:27:31 -0300

    Create SaldoInsuficienteException.cs

---
+++ TratamentoDeExceções/TratamentoDeExcecoes/ExececoesPersonalizadas/SaldoInsuficienteException.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace ExececoesPersonalizadas
+{
+    internal partial class Program
+    {
+        public class SaldoInsuficienteException : Exception
+        {
+            public SaldoInsuficienteException()
+            {
+            }
+
+            public SaldoInsuficienteException(string? message) : base(message)
+            {
+            }
+
+            public SaldoInsuficienteException(string? message, Exception? innerException) : base(message, innerException)
+            {
+            }
+
+            protected SaldoInsuficienteException(SerializationInfo info, StreamingContext context) : base(info, context)
+            {
+            }
+        }
+    }
+}
