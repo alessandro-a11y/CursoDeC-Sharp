c926520a5b94eb5ce0f081e165d8fb4e8e4fb932
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-06 23:27:50 -0300

    Create Program.cs

---
+++ Delegates/Delegates/Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        // Delegate
+        public delegate void Operacao(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+            // Forma explícita (raiz)
+            Operacao op = new Operacao(calc.Somar);
+            op.Invoke(10, 5);
+
+            op = new Operacao(calc.Multiplicar);
+            op.Invoke(10, 5);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Calculadora
+    {
+        public void Somar(int a, int b)
+        {
+            Console.WriteLine("Soma: " + (a + b));
+        }
+
+        public void Multiplicar(int a, int b)
+        {
+            Console.WriteLine("Multiplicação: " + (a * b));
+        }
+    }
+}
