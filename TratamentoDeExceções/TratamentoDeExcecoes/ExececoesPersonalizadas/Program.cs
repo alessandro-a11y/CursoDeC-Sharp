ea12825447fb40a1c1025567e09c4f1aeb7fc6c8
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-06 23:27:34 -0300

    Update Program.cs

--- TratamentoDeExceções/TratamentoDeExcecoes/ExececoesPersonalizadas/Program.cs
+++ TratamentoDeExceções/TratamentoDeExcecoes/ExececoesPersonalizadas/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 
 namespace ExececoesPersonalizadas
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
@@ -21,62 +20,6 @@ namespace ExececoesPersonalizadas
             {
                 Console.WriteLine("Erro ao sacar: " + ex.Message);
             }
-        }
-        public class SaldoInsuficienteException : Exception
-        {
-            public SaldoInsuficienteException()
-            {
-            }
-
-            public SaldoInsuficienteException(string? message) : base(message)
-            {
-            }
-
-            public SaldoInsuficienteException(string? message, Exception? innerException) : base(message, innerException)
-            {
-            }
-
-            protected SaldoInsuficienteException(SerializationInfo info, StreamingContext context) : base(info, context)
-            {
-            }
-        }
-        public class Conta
-        {
-            public Conta(int numero, string? titular, decimal saldo)
-            {
-                Numero = numero;
-                Titular = titular;
-                Saldo = saldo;
-            }
-
-            public int Numero { get; set; }
-            public string? Titular { get; set; }
-            public decimal Saldo { get; set; }
-
-            public decimal Depositar(decimal valor)
-            {
-                Saldo += valor;
-                Console.WriteLine($"Despositou: {valor}");
-                return Saldo;
-            }
-            public decimal Sacar(decimal valor)
-            {
-                Console.WriteLine($"Sacou: {valor}");
-                if (Saldo < valor)
-                {
-                    throw new SaldoInsuficienteException("Saldo insuficiente");
-                }
-                else
-                {
-                    Saldo -= valor;
-                    return Saldo;
-                }
-            }
-            public override string ToString()
-            {
-                return $"Conta: {Numero} : {Titular} - Saldo = {Saldo}";
-            }
-
         }
     }
 }
