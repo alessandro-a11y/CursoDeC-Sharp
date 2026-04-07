e8becfbb35bc3863d12b0c6c5ca3e345745703c9
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-06 23:27:25 -0300

    Create Program.cs

---
+++ TratamentoDeExceções/TratamentoDeExcecoes/Exercicio1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * - Escreva um program onde o usuário  é solicitado a informar um valor via teclado e armazenar o valor na 
+                 variável entrada do tipo string onde tem que verificar 3 condições diferentes:
+             */
+            try
+            {
+                Console.WriteLine("Insira uma palavra: ");
+                string entrada = Console.ReadLine();
+
+                if (entrada == null)
+                {
+                    throw new ArgumentNullException( "A entrada não pode ser nula.");
+                }
+                if (entrada == "")
+                {
+                    throw new ArgumentException("A entrada não pode ser vazia.");
+                }
+
+
+                Console.WriteLine($"Entrada válida: {entrada}");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocorreu um erro inesperado: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("fim do programa");
+            }
+        }
+    }
+}
