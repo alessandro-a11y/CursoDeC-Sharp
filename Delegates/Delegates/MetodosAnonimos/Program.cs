8e50fbe84f68ceff64fb89bd8f28fe42af06d828
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-08 23:16:08 -0300

    Create Program.cs

---
+++ Delegates/Delegates/MetodosAnonimos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MetodosAnonimos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nomes = new List<string>
+            {
+                "Maria", "Miriam", "Paulo", "Carlos"
+            };
+
+            string resultado = nomes.Find(delegate (string nome);
+            {
+                return nome.Equals("Paulo");
+            }
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
