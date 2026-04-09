527f3da036fcbfc8040594fb1793838ad7378586
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-08 23:15:53 -0300

    Create Program.cs

---
+++ Delegates/Delegates/MulticastDelegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace MulticastDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuDelegate delegateMulticast = new MeuDelegate(Metodo1);
+            delegateMulticast += Metodo2;
+
+            delegateMulticast("Olá, mundo!");
+
+
+
+            static void Metodo1(string mensagem)
+            {
+                Console.WriteLine("Método 1: " + mensagem);
+            }
+            static void Metodo2(string mensagem)
+            {
+                Console.WriteLine("Método 2: " + mensagem);
+            }
+        }
+        public delegate void MeuDelegate(string mensagem);
+    }
+}
