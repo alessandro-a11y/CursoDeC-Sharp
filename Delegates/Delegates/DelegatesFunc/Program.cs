7412c4a969419ce61e61652cd3a0f845dfb1e703
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-12 10:47:11 -0300

    Create Program.cs

---
+++ Delegates/Delegates/DelegatesFunc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> raizQuadrada = n => Math.Sqrt(n);
+
+            Console.WriteLine("Digite um número:");
+            double numero = double.Parse(Console.ReadLine());
+
+            double resultado = raizQuadrada(numero);
+            Console.WriteLine($"A raiz quadrada de {numero} é {resultado}");
+        }
+    }
+}
