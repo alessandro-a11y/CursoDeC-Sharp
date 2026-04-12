a54750a3d958a3299dafe1aec4c76c2ecbf43cc9
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-12 10:46:59 -0300

    Create Program.cs

---
+++ Delegates/Delegates/Exercicio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Predicate<int> ehPar = n => n % 2 == 0 ? true : false;
+
+            if(ehPar(numero))
+            {
+                Console.WriteLine("O número é par.");
+            }
+            else
+            {
+                Console.WriteLine("O número é ímpar.");
+            }
+        }
+    }
+}
