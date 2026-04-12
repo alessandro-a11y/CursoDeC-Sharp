df6880f0218275dc58418972d617b7971bbe07e0
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-12 10:46:54 -0300

    Create Program.cs

---
+++ Delegates/Delegates/Exercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número inteiro");
+            int numero = int.Parse(Console.ReadLine());
+
+            Action<int> dobra = n => Console.WriteLine($"O dobro de {n} é {n * 2}");
+        }
+    }
+}
