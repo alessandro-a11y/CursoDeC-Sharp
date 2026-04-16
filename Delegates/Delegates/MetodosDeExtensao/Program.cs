ff4b023b7b95b145e2ec65d7cbc43a56b0f81761
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-16 16:42:21 -0300

    Create Program.cs

---
+++ Delegates/Delegates/MetodosDeExtensao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosDeExtensao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "Olá, mundo!";
+            string textoInvertido = texto.InverteString();
+            Console.WriteLine($"Texto original: {texto}");
+            Console.WriteLine($"Texto invertido: {textoInvertido}");
+        }
+    }
+    public static class StringExtensions
+    {
+        public static string InverteString(this string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
