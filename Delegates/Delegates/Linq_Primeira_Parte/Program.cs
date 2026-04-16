6986d591487d7a9d75237358632cd9a6de6bd342
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-16 16:42:27 -0300

    Create Program.cs

---
+++ Delegates/Delegates/Linq_Primeira_Parte/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosDeExtensao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nomes = new List<string>()
+            {
+                "João", "Maria", "Pedro", "Ana" 
+            };
+            var resultado = from n in nomes
+                            where n.Contains("o")
+                            select n;
+
+            foreach (var item in resultado)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<string> nomes2 = new List<string>()
+            {
+                "João", "Maria", "Pedro", "Ana" 
+            };
+
+            var resultado2 = nomes2.Where(n => n.Contains("o"));
+            foreach (var item1 in resultado2)
+            {
+                Console.WriteLine(item1);
+            }
+
+        }
+    }
+
+}
