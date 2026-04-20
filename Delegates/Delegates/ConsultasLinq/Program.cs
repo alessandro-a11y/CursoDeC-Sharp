1cd6ddc8768e752b6473ce8146c27bea548d5843
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-20 13:53:18 -0300

    Create Program.cs

---
+++ Delegates/Delegates/ConsultasLinq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsultasLinq
+{
+    internal class Program
+    {
+        public delegate int OperacaoMatematica(int a, int b);
+        static void Main(string[] args)
+        {
+            OperacaoMatematica operacao = Somar;
+            int resultado = operacao.Invoke(5, 5);
+            Console.WriteLine($"Resultado: {resultado}");
+        }
+        public static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
