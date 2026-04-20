305c8cf981e4c714439b572acdcae539f7c2ae11
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-20 13:53:10 -0300

    Create Email.cs

---
+++ Delegates/Delegates/Eventos/Email.cs
@@ -0,0 +1,10 @@
+﻿namespace Eventos
+{
+    class Email
+    {
+        public static void Enviar(object? sender, PedidosEventArgs e)
+        {
+            Console.WriteLine($"\nEnviando um email para {e.Email}");
+        }
+    }
+}
