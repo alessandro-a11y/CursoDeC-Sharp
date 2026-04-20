3a035e0af8f1b5812507e10e7b0c44057aecf16a
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-20 13:52:51 -0300

    Update Program.cs

--- Delegates/Delegates/Eventos/Program.cs
+++ Delegates/Delegates/Eventos/Program.cs
@@ -16,35 +16,4 @@ namespace Eventos
             pedido.CriarPedido("exemplo@email.com", "123456789");
         }
     }
-    class PedidosEventArgs : EventArgs
-    {
-        public string? Email { get; set; }
-        public string? Telefone { get; set; }
-    }
-    class Pedido
-    {
-        public event EventHandler<PedidosEventArgs>? OnCriarPedido;
-
-        public void CriarPedido(string email, string telefone)
-        {
-            Console.WriteLine("\nPedido criado");
-
-            // Forma moderna
-            OnCriarPedido?.Invoke(this, new PedidosEventArgs { Email = email, Telefone = telefone });
-        }
-    }
-    class Email
-    {
-        public static void Enviar(object? sender, PedidosEventArgs e)
-        {
-            Console.WriteLine($"\nEnviando um email para {e.Email}");
-        }
-    }
-    class SMS
-    {
-        public static void Enviar(object? sender, PedidosEventArgs e)
-        {
-            Console.WriteLine($"\nEnviando um SMS para {e.Telefone}");
-        }
-    }
 }
