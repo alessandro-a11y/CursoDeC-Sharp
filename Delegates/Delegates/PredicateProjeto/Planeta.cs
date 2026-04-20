407fbc6cd3b158d27bf7ea60e410d1a88a51476c
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-20 13:52:18 -0300

    Create Planeta.cs

---
+++ Delegates/Delegates/PredicateProjeto/Planeta.cs
@@ -0,0 +1,9 @@
+﻿namespace PredicateProjeto
+{
+    public class Planeta
+    {
+        public string Nome { get; set; }
+        public double Massa { get; set; }
+        public double Diametro { get; set; }
+    }
+}
