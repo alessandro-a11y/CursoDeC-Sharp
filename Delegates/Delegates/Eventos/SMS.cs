0c3d8edd94eef9c5d5531323e46247522052fa8c
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-20 13:52:47 -0300

    Create SMS.cs

---
+++ Delegates/Delegates/Eventos/SMS.cs
@@ -0,0 +1,10 @@
+﻿namespace Eventos
+{
+    class SMS
+    {
+        public static void Enviar(object? sender, PedidosEventArgs e)
+        {
+            Console.WriteLine($"\nEnviando um SMS para {e.Telefone}");
+        }
+    }
+}
