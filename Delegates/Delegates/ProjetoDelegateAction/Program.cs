365911138ebca55ec2f6aa23f4a53d753f9d1959
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-20 13:52:06 -0300

    Create Program.cs

---
+++ Delegates/Delegates/ProjetoDelegateAction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace ProjetoDelegateAction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = Enumerable.Range(1, 20).ToList();
+
+            Action<int> imprimirSePar = n =>
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine(n);
+                }
+            };
+            Console.WriteLine("---------- Lista de números pares ----------".PadLeft(30));
+            numeros.ForEach(imprimirSePar);
+        }
+    }
+}
