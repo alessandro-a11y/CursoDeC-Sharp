e5931e598a579f0b66627d95a5518176a7a9fb36
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-20 13:52:38 -0300

    Create ListExtensions.cs

---
+++ Delegates/Delegates/MetodoDeExtensao/ListExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace MetodoDeExtensao
+{
+    public static class ListExtensions
+    {
+        public static int SomarImpares(this List<int> lista)
+        {
+            int soma = 0;
+            foreach (var numero in lista)
+            {
+                if (numero % 2 != 0)
+                {
+                    soma += numero;
+                }
+            }
+            return soma;
+        }
+    }
+}
