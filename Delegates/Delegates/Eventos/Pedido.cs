ad0667e96078eb998e555f5d2d5217cebdbfbde5
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-20 13:53:00 -0300

    Create Pedido.cs

---
+++ Delegates/Delegates/Eventos/Pedido.cs
@@ -0,0 +1,15 @@
+﻿namespace Eventos
+{
+    class Pedido
+    {
+        public event EventHandler<PedidosEventArgs>? OnCriarPedido;
+
+        public void CriarPedido(string email, string telefone)
+        {
+            Console.WriteLine("\nPedido criado");
+
+            // Forma moderna
+            OnCriarPedido?.Invoke(this, new PedidosEventArgs { Email = email, Telefone = telefone });
+        }
+    }
+}
