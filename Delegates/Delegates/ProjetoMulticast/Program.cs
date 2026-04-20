50c318492830675bbfd3ba0107db0bbb74db498f
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-20 13:51:07 -0300

    Create Program.cs

---
+++ Delegates/Delegates/ProjetoMulticast/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetoMulticast
+{
+    internal class Program
+    {
+        public delegate void MulticastBoasVindas();
+        static void Main(string[] args)
+        {
+            MulticastBoasVindas boasVindas = BoasVindasPT;
+            boasVindas += BoasVindasEN;
+
+            boasVindas();
+        }
+        static void BoasVindasPT()
+        {
+            Console.WriteLine("Bem-vindo ao Projeto Multicast!");
+        }
+        static void BoasVindasEN()
+        {
+            Console.WriteLine("Welcome to the Multicast Project!");
+        }
+    }
+}
