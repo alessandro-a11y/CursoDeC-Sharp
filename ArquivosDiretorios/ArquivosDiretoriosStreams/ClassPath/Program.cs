fecaafc1a5d826188ae6c48bc88480cbc92c1b46
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-26 14:55:11 -0300

    Create Program.cs

---
+++ ArquivosDiretorios/ArquivosDiretoriosStreams/ClassPath/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace ArquivosDiretoriosStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminho = "C:\\Users\\Public\\Documents\\ArquivoDeOrigem.txt";
+
+            char dirSeparador = Path.DirectorySeparatorChar;
+            Console.WriteLine("O separador de diretório padrão é: " + dirSeparador);
+
+
+            string[] diretorios = caminho.Split(dirSeparador);
+
+            Console.WriteLine("Os diretórios e arquivo no caminho são: ");
+            foreach (var item in diretorios)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
