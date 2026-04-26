b7619a54ca1c15a78a5e862313daa2513e5e62d7
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-26 14:55:18 -0300

    Create Program.cs

---
+++ ArquivosDiretorios/ArquivosDiretoriosStreams/ArquivosDiretoriosStreams/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace ArquivosDiretoriosStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string CaminhoDeOrigem = @"C:\Users\Public\Documents\ArquivoDeOrigem.txt";
+
+            File.WriteAllText(CaminhoDeOrigem, "Olá, este é um arquivo de origem!");
+
+            string novoTexto = File.ReadAllText(CaminhoDeOrigem) + "\nEste é um novo texto adicionado ao arquivo de origem.";
+
+            File.AppendAllText(CaminhoDeOrigem, novoTexto);
+
+            string conteudoFinal = File.ReadAllText(CaminhoDeOrigem);
+            Console.WriteLine(conteudoFinal);
+        }
+    }
+}
