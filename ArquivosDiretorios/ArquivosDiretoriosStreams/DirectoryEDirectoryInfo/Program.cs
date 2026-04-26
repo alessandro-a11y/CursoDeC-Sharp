50dc875c7567cc4cfe6873b281ed9f171355b874
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-26 14:55:02 -0300

    Create Program.cs

---
+++ ArquivosDiretorios/ArquivosDiretoriosStreams/DirectoryEDirectoryInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace ArquivosDiretoriosStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var caminhoDiretorio = @"C:\Users\Public\Documents\NovoDiretorio";
+
+            try
+            {
+                if (!Directory.Exists(caminhoDiretorio))
+                {
+                    Directory.CreateDirectory(caminhoDiretorio);
+                    Console.WriteLine($"Diretório criado em: {caminhoDiretorio}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"O diretório já existe em: {caminhoDiretorio}");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
