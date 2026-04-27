c5991fd29a3b63b71b80805fcc98187705d94938
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-27 12:18:26 -0300

    Create Program.cs

---
+++ ArquivosDiretorios/ArquivosDiretoriosStreams/Exercicio8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o caminho do diretório: ");
+            string caminho = Console.ReadLine();
+
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(caminho);
+
+                if (!dir.Exists)
+                {
+                    Console.WriteLine("Diretório não encontrado.");
+                    return;
+                }
+
+                DirectoryInfo[] subDiretorios = dir.GetDirectories();
+
+                foreach (DirectoryInfo subDir in subDiretorios)
+                {
+                    Console.WriteLine(subDir.Name);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
