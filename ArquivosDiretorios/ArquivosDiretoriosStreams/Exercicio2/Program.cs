b10b98995c58ac196c5f7e39edc2007be27b6575
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-04-27 12:19:09 -0300

    Create Program.cs

---
+++ ArquivosDiretorios/ArquivosDiretoriosStreams/Exercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o caminho: ");
+            string caminho = Console.ReadLine();
+
+            try
+            {
+                if (File.Exists(caminho))
+                {
+                    DateTime datacricao = File.GetCreationTime(caminho);
+
+                    Console.WriteLine("Arquivo Encontrado");
+                    Console.WriteLine($"Data de criação: {datacricao}");
+                }
+                else
+                {
+                    Console.WriteLine("Arquivo não encontrado");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
