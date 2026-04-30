b0fdf00228f6bbe2a6062a5248e4fd79f7c9a326
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-04-30 18:17:17 -0300

    Create Program.cs

---
+++ ArquivosDiretorios/ArquivosDiretoriosStreams/SerializaçãoDeserializaçãoEBinaria/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializacaoBinaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno(101, "Maria", "maria@yahoo.com", 17);
+
+            var caminhoArquivo = @"C:\temp\aluno.bin";
+
+            using (FileStream stream = new FileStream(caminhoArquivo,
+                FileMode.Create,
+                FileAccess.ReadWrite))
+            {
+                var bf = new BinaryFormatter();
+                bf.Serialize(stream, aluno1);
+            }
+        }
+    }
+}
