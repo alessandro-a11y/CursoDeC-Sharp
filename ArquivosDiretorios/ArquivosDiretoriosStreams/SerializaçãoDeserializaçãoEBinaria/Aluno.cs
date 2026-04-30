12266864effe3a90988830e0d3b991159e8cb550
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-04-30 18:17:22 -0300

    Create Aluno.cs

---
+++ ArquivosDiretorios/ArquivosDiretoriosStreams/SerializaçãoDeserializaçãoEBinaria/Aluno.cs
@@ -0,0 +1,22 @@
+﻿namespace SerializacaoBinaria
+{
+    [Serializable]
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        [NonSerialized]
+        public int Idade;
+
+
+        public Aluno(int id, string nome, string email, int idade)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Idade = idade;
+        }
+    }
+}
