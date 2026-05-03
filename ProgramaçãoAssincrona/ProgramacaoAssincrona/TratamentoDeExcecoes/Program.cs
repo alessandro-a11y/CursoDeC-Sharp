5c08002fd53b5617d844fdebcf54eaac187f0259
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-03 20:16:53 -0300

    Create Program.cs

---
+++ ProgramaçãoAssincrona/ProgramacaoAssincrona/TratamentoDeExcecoes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        TesteAsync t = new();
+
+        try
+        {
+            t.ChamaTarefaAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            Console.WriteLine("Este bloco não será executado");
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+class TesteAsync
+    {
+        public Task MinhaTarefaAsync()
+        {
+            return Task.Run(() =>
+            {
+                Task.Delay(2000);
+                throw new Exception("Ocorreu um erro na tarefa!");
+
+            });
+        }
+        public async void ChamaTarefaAsync()
+        {
+            await MinhaTarefaAsync();
+        }
+    }
+}
