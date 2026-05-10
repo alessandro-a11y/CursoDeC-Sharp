aee69ee7c6b5338c4ba1f0b5f552eafdbb360d3f
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-10 17:35:01 -0300

    Create Program.cs

---
+++ ProgramaçãoAssincrona/ProgramacaoAssincrona/StreamAsync/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await foreach(var item in GeraMeses())
+        {
+            Console.WriteLine(item);
+        }
+    }
+    static private async IAsyncEnumerable<string> GeraMeses()
+    {
+        yield return "Janeiro";
+        yield return "Fevereiro";
+        yield return "Março";
+        yield return "Abril";
+        yield return "Maio";
+        yield return "Junho";
+        await Task.Delay(5000);
+        yield return "Julho";
+        yield return "Agosto";
+        yield return "Setembro";
+        yield return "Outubro";
+        yield return "Novembro";
+        yield return "Dezembro";
+    }
+}
