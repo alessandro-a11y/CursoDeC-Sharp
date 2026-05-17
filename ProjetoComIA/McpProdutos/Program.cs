099c773a1ecb1baea943f888ef7466ab0a3994e2
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-17 17:20:22 -0300

    Create Program.cs

---
+++ ProjetoComIA/McpProdutos/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using McpProdutos.Tools;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+var app = builder.Build();
+
+await app.RunAsync();
