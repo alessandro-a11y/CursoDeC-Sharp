8b8d73dfc888ede4c5163e56e124ecb2e97becf6
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-05-24 17:10:52 -0300

    fix: treinamento com agrupamentos e junções

---
+++ LINQ/LINQ/Agrupamentos/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Agrupamentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var alunos = FonteDeDados.ObterAlunos();
+
+            var grupos = alunos.GroupBy(x => new { x.Curso, x.Sexo })
+                                         .OrderBy(g => g.Key.Curso)
+                                         .ThenBy(g => g.Key.Sexo)
+                                         .Select(g => new
+                                         {
+                                             Curso = g.Key.Curso,
+                                             Sexo = g.Key.Sexo,
+                                             Alunos = g.Select(a => a.Nome).ToList()
+                                         });
+
+
+            foreach (var grupo in grupos)
+            {
+                Console.WriteLine($"Curso: {grupo.Curso}, Sexo: {grupo.Sexo}");
+                Console.WriteLine("Alunos:");
+                foreach (var aluno in grupo.Alunos)
+                {
+                    Console.WriteLine($" - {aluno}");
+                }
+            }
+        }
+        public class FonteDeDados
+        {
+            public class Aluno
+            {
+                public string Nome { get; set; }
+                public string Curso { get; set; }
+                public int Idade { get; set; }
+                public string Sexo { get; set; }
+
+                public Aluno(string nome, string curso, string sexo, int idade)
+                {
+                    Nome = nome;
+                    Curso = curso;
+                    Idade = idade;
+                    Sexo = sexo;
+                }
+            }
+            public static List<Aluno> ObterAlunos()
+            {
+                return new List<Aluno>()
+            {
+             new Aluno("João", "Java", "Masculino", 20),
+             new Aluno("Maria", "C#", "Feminino", 22)
+            };
+            }
+        }
+    }
+}
--- LINQ/LINQ/LINQ/Program.cs
+++ LINQ/LINQ/LINQ/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> frutas = new List<string>()
+            {
+                "Banana",
+                "Maçã",
+                "Laranja",
+                "Abacaxi",
+                "Uva"
+            };
+
+            // query syntax
+            var resultado = from fruta in frutas
+                            where fruta.Contains("a")
+                            select fruta;
+
+            foreach (var item in resultado)
+            {
+                Console.WriteLine(item);
+            }
+
+            // method syntax
+            var resultado2 = frutas.Where(fruta => fruta.Contains("a"));
+            foreach (var item in resultado2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
