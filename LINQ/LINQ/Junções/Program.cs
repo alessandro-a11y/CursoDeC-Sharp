ea0d142d64f75b92acf18f3111cb64a2d321b8d4
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-06-03 10:40:06 -0300

    Create Program.cs

---
+++ LINQ/LINQ/Junções/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Agrupamentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public class FonteDeDados
+        {
+            public class Aluno
+            {
+                public string Nome { get; set; }
+                public string Curso { get; set; }
+                public int Idade { get; set; }
+                public string Sexo { get; set; }
+
+                public Aluno(string nome, string curso, string sexo, int idade)
+                {
+                    Nome = nome;
+                    Curso = curso;
+                    Idade = idade;
+                    Sexo = sexo;
+                }
+            }
+            public static List<Aluno> ObterAlunos()
+            {
+                return new List<Aluno>()
+            {
+             new Aluno("João", "Java", "Masculino", 20),
+             new Aluno("Maria", "C#", "Feminino", 22)
+            };
+            }
+        }
+    }
+}
