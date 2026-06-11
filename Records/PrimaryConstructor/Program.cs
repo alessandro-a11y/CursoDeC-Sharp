075f84826c0515878a6e3e6949387bf6bc89d651
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-06-11 13:09:50 -0300

    adiciona novo conteúdo

---
+++ Records/PrimaryConstructor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryConstructor
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Pedido(Guid clienteID, List<string> itens, DateTime data)
+    {
+        public Guid ClienteId { get; } = clienteID != Guid.Empty
+            ? clienteID
+            : throw new ArgumentException("Cliente inválido");
+
+        public List<string> Itens { get; } = itens is { Count: > 0 }
+            ? itens
+            : throw new ArgumentException("Itens obrigatórios");
+
+        public DateTime Data { get; } = data != default
+            ? data
+            : throw new ArgumentException("Data inválida");
+    }
+}
